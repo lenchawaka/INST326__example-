b75139ab614777b63437d5f0a2f93e21e339e982
Len Chawaka <lchawak2@terpmail.umd.edu>
2023-04-03 17:52:44 -0400

    added test

---
+++ example.py
@@ -0,0 +1,16 @@
+""""
+this is a simple example of using git for inst 326
+"""
+
+
+class person:
+
+    """
+    a person in the class name
+
+    Attributes:
+            name (str): the person's name
+            conneciton:  connection to the person
+
+
+    """
